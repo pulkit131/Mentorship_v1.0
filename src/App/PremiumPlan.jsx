4bbacbd2bf2d8a9e8bdf7a34613d0d94cea8a860
unknown <hardikjalan2005@gmail.com>
2025-06-06 02:24:15 +0530

    Update Function name

--- src/App/PremiumPlan.jsx
+++ src/App/PremiumPlan.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TickBox from "../assets/TickBox.png";
 
-const PremiumCard = () => {
+const PremiumPlan = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white px-4 py-12">
       <h1 className="text-4xl font-bold text-black mb-5 text-center">
