9277693fec39815630e86e58519304cff469c1de
Raj Inder Singh Gulati <rajisflash@Rajs-MacBook-Air-2.local>
2025-07-22 21:19:32 +0530

    updated url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const axiosInstance = axios.create(
     {
-        baseURL: "http://147.93.105.148:3000/api",
+        baseURL: "https://api.letsgetmentor.com/api",
         withCredentials: true //we want to send cookies throughout 
     }
 )
