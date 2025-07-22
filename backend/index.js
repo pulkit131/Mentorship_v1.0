a89c870570e62ac10f886faef1b22716f07efb71
Raj Inder Singh Gulati <rajisflash@Rajs-MacBook-Air-2.local>
2025-07-22 20:16:59 +0530

    fixing port

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
